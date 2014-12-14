8f6594dcdcb43625f68e00f503aa343c072e7fa7
Jasper <japsai@gmail.com>
2014-12-14 00:56:26 -0800

    Setup.py and requirements files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(name='niprov',
+      version='0.0',
+      description='provenance for neuroimaging data',
+      packages=find_packages(),
+      test_suite="",
+      )

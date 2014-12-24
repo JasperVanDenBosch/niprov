f981143f5bfe030a1451bb1c28484be8087b8bf1
Jasper <japsai@gmail.com>
2014-12-23 19:10:39 -0800

    Inspection obtains general_info

--- niprov/inspection.py
+++ niprov/inspection.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+from datetime import datetime
 from niprov.commandline import Commandline
 from niprov.dependencies import Dependencies
 
 
 def inspect(fpath, listener=Commandline(), libs=Dependencies()):
+    provenance = {}
     if libs.hasDependency('nibabel'):
-        libs.nibabel.load(fpath)
+        img = libs.nibabel.load(fpath)
+        provenance['subject'] = img.header.general_info['patient_name']
+        provenance['protocol'] = img.header.general_info['protocol_name']
+        acqstring = img.header.general_info['exam_date']
+        dateformat = '%Y.%m.%d / %H:%M:%S'
+        provenance['acquired'] = datetime.strptime(acqstring,dateformat)
+    return provenance
 
     
--- tests/inspection.py
+++ tests/inspection.py
@@ -1,12 +1,13 @@
 import unittest
 from mock import Mock
+from datetime import datetime
 
 class InspectionTests(unittest.TestCase):
 
     def test_Loads_file_with_nibabel(self):
         import niprov.inspection
         log = Mock()
-        libs = Mock()
+        libs = self.setupNibabel()
         libs.hasDependency.return_value = True
         niprov.inspection.inspect('/p/f1.x', listener=log, libs=libs)
         libs.nibabel.load.assert_any_call('/p/f1.x')
@@ -14,8 +15,28 @@ class InspectionTests(unittest.TestCase):
     def test_Doesnt_use_nibabel_if_not_installed(self):
         import niprov.inspection
         log = Mock()
-        libs = Mock()
+        libs = self.setupNibabel()
         libs.hasDependency.return_value = False
         niprov.inspection.inspect('/p/f1.x', listener=log, libs=libs)
         self.assertRaises(AssertionError,
             libs.nibabel.load.assert_any_call, '/p/f1.x')
+
+    def test_Gets_basic_info_from_nibabel_and_returns_it(self):
+        import niprov.inspection
+        log = Mock()
+        libs = self.setupNibabel()
+        libs.hasDependency.return_value = True
+        out = niprov.inspection.inspect('/p/f1.x', listener=log, libs=libs)
+        self.assertEqual(out['subject'], 'John Doeish')
+        self.assertEqual(out['protocol'], 'T1 SENSE')
+        self.assertEqual(out['acquired'], datetime(2014, 8, 5, 11, 27, 34))
+
+    def setupNibabel(self):
+        libs = Mock()
+        img = Mock()
+        img.header.general_info = {
+            'exam_date':'2014.08.05 / 11:27:34',
+            'protocol_name':'T1 SENSE',
+            'patient_name':'John Doeish'}
+        libs.nibabel.load.return_value = img
+        return libs

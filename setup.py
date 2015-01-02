218378a777c29c22bd43801d1122419198a79995
Jasper <japsai@gmail.com>
2015-01-01 17:14:04 -0800

    Release 0.0 configured and uploaded to pypi.

--- setup.py
+++ setup.py
@@ -5,9 +5,16 @@ from setuptools import setup, find_packages
 
 setup(name='niprov',
       version='0.0',
+      author='Jasper J.F. van den Bosch',
+      author_email='japsai@gmail.com',
       description='provenance for neuroimaging data',
       packages=find_packages(),
+      url = 'https://github.com/ilogue/niprov',
       test_suite="tests",
       scripts=['executables/discover', 'executables/provenance'],
-      zip_safe=False
+      zip_safe=False,
+      classifiers=[
+            'License :: OSI Approved :: BSD License',
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering']
       )

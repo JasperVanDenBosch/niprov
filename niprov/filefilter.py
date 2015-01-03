98ed896d61b7f75c725a2e30556e7bae7498e2be
Jasper <japsai@gmail.com>
2015-01-02 22:06:18 -0800

    Made sure that creating a Filefilter object does not raise an exception if filter file is not available. This happens e.g. on readthedocs.org

--- niprov/filefilter.py
+++ niprov/filefilter.py
@@ -10,9 +10,14 @@ defaultfilterfile = os.path.join(packageroot,'discovery-filter.txt')
 class FileFilter(object):
 
     def __init__(self, filesys=Filesystem()):
-        self.filters = filesys.readlines(defaultfilterfile)
+        try:
+            self.filters = filesys.readlines(defaultfilterfile)
+        except:
+            self.filters = None
 
     def include(self, filepath):
+        if not self.filters:
+            raise ValueError('Was not able to load filter file.')
         for filt in self.filters:
             if filt in filepath:
                 return True

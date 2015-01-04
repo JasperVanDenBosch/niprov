b2169180c20a16d4fb7226667db624fd19b55f01
Jasper <japsai@gmail.com>
2015-01-04 13:56:49 -0800

    Switched to 'sphinxcontrib.napoleon'as readthedocs uses Sphinx 1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.napoleon',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

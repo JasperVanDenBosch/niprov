80034fa14a3d9fb903aef0c105695aae18bcf3a3
Jasper van den Bosch <jasperb@uw.edu>
2015-02-20 19:02:01 -0800

    [doc] Docstring for discovery.

--- niprov/discovery.py
+++ niprov/discovery.py
@@ -10,6 +10,16 @@ from niprov.jsonfile import JsonFile
 
 def discover(root, filefilter=FileFilter(), filesys=Filesystem(), 
         listener=Commandline(), repository=JsonFile()):
+    """
+    Search a directory for image files, inspect them and store provenance.
+
+    Files are only included if they match the filters in the 
+    'discovery-filter.txt' file.
+    If a file is already known, it will be ignored and the listener informed.
+
+    Args:
+        root (str): The top directory in which to look for new files.
+    """
     dirs = filesys.walk(root)
     for (root, sdirs, files) in dirs:
         for filename in files:

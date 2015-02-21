232670e1b83a980e49496b829954c653408e355e
Jasper van den Bosch <jasperb@uw.edu>
2015-02-20 18:53:02 -0800

    Storage test will no longer write to production database, but to temporary files.

--- tests/storage.py
+++ tests/storage.py
@@ -9,12 +9,18 @@ import random
 
 class StorageTests(unittest.TestCase):
 
+    def setUp(self):
+        r = random.randint(1000,9999)
+        self.templocation = '/var/tmp/provenance-{0}.json'.format(r)
+
     def test_Can_add_file_and_retrieve_it(self):
         from niprov.jsonfile import JsonFile
         repo = JsonFile()
+        repo.datafile = self.templocation
         provenance = self.sampleProvenanceRecord()
         repo.add(provenance)
         repo2 = JsonFile()
+        repo2.datafile = self.templocation
         out = repo2.byPath(provenance['path'])
         self.assertEqual(out, provenance)
 
@@ -24,6 +30,7 @@ class StorageTests(unittest.TestCase):
         filesys.read.side_effect = IOError(
             "[Errno 2] No such file or directory: 'provenance.json'")
         repo = JsonFile(filesys)
+        repo.datafile = self.templocation
         out = repo.all()
         self.assertEqual(out, [])
 
@@ -32,15 +39,18 @@ class StorageTests(unittest.TestCase):
         serializer = Mock()
         serializer.deserializeList.return_value = []
         repo = JsonFile(json=serializer)
+        repo.datafile = self.templocation
         with self.assertRaises(IndexError):
             repo.byPath('nothere')
 
     def test_Can_add_file_and_ask_whether_its_known(self):
         from niprov.jsonfile import JsonFile
         repo = JsonFile()
+        repo.datafile = self.templocation
         provenance = self.sampleProvenanceRecord()
         repo.add(provenance)
         repo2 = JsonFile()
+        repo2.datafile = self.templocation
         self.assertFalse(repo2.knowsByPath('nonexisting'))
         self.assertTrue(repo2.knowsByPath(provenance['path']))
 

df6feee9a77742c8ebb2f72e9ad58a5222035b93
Jasper <japsai@gmail.com>
2015-03-04 00:10:22 -0800

    First acceptance test and added test data..

---
+++ acceptance/test_add.py
@@ -0,0 +1,19 @@
+import unittest
+import os, shutil
+
+
+class ApiTests(unittest.TestCase):
+
+    def setUp(self):
+        self.dbpath = os.path.expanduser(os.path.join('~','provenance.json'))
+        if os.path.exists(self.dbpath):
+            shutil.move(self.dbpath, self.dbpath.replace('.json','.backup.json'))
+
+    def tearDown(self):
+        if os.path.exists(self.dbpath):
+            os.remove(self.dbpath)
+
+    def test_Discover(self):
+        import niprov
+        niprov.discover('testdata')
+
---
+++ testdata/README.md
@@ -0,0 +1,9 @@
+test data
+=========
+
+Is a collection of imaging data collected among others at Philips Intera Achieva 3T MR scanner at Dartmouth Brain Imaging Center.
+
+License
+-------
+
+This dataset was made available under the Public Domain Dedication and License v1.0 whose full text can be found at: http://www.opendatacommons.org/licenses/pddl/1.0/

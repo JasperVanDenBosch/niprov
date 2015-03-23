82459b67d59ec5229e3745a7db197b08a294008c
Jasper van den Bosch <jasperb@uw.edu>
2015-03-23 14:20:30 -0700

    StdOut exporter can now also publish a narrative format.

--- niprov/stdout.py
+++ niprov/stdout.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+from __future__ import print_function
 import copy
 from niprov.exporter import BaseExporter
 
@@ -45,3 +46,13 @@ class StandardOutputExporter(BaseExporter):
         tmp = ('{0[acquired]!s}  {0[subject]!s:12} {0[protocol]!s:24} '
             '{0[dimensions]!s:20} {0[path]!s:24}')
         print(tmp.format(provcopy))
+
+    def exportNarrative(self, provenance):
+        """Publish provenance in a 'story' format in the terminal.
+
+        Args:
+            provenance (dict or list): Provenance either for one file or several.
+        """
+        print('\n')
+        print(self.narrator.narrate(provenance))
+        print('\n')
---
+++ tests/test_StdoutExporter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from unittest import TestCase
+from mock import Mock, sentinel, patch
+
+
+class StdoutExporterTests(TestCase):
+
+    def test_For_narrative_form_Returns_narrator_output(self):
+        mprint = Mock()
+        from niprov.stdout import StandardOutputExporter
+        narrator = Mock()
+        with patch('__builtin__.print') as mprint:
+            exporter = StandardOutputExporter(form='narrative', 
+                narrator=narrator)
+            out = exporter.exportNarrative(sentinel.one)
+            narrator.narrate.assert_called_with(sentinel.one)
+            mprint.assert_called_with(narrator.narrate())
+

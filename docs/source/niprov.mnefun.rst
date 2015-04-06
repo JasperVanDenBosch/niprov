b68f1cb1d2c6bc4938d679b9bd993ae8a5a39806
Jasper van den Bosch <jasperb@uw.edu>
2015-04-06 14:32:37 -0700

    [doc] Added docstrings to mnefun module.

---
+++ docs/source/niprov.mnefun.rst
@@ -0,0 +1,7 @@
+niprov.mnefun module
+====================
+
+.. automodule:: niprov.mnefun
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- niprov/mnefun.py
+++ niprov/mnefun.py
@@ -1,8 +1,23 @@
+# -*- coding: utf-8 -*-
+"""mnefun support module
+
+This module provides handlers to attach to mnefun events.
+
+::
+
+    import mnefun
+    import niprov.mnefun
+    params = mnefun.Params()
+    params.on_process = niprov.mnefun.handler
+
+"""
 import os
 from niprov.discovery import discover
 
 
 def handler(text, func, out, params):
+    """mnefun on_process handler
+    """
     for subj in params.subjects:
         subjrawdir = os.path.join(params.work_dir, subj, params.raw_dir)
         discover(subjrawdir)

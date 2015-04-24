787a6506bc89a19fc05fd61479ca098066edc917
Jasper <japsai@gmail.com>
2015-04-24 00:09:51 -0700

    Patch version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except (IOError, ImportError):
    README = ''
 
 setup(name='niprov',
-      version='0.1.post8',
+      version='0.1.post9',
       author='Jasper J.F. van den Bosch',
       author_email='japsai@gmail.com',
       description='provenance for neuroimaging data',

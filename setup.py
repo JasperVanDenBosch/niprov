37dce5201650b34bbfcaad33b44ac167b109f591
Jasper van den Bosch <jasperb@uw.edu>
2015-09-14 14:23:21 -0700

    Bumped minor version; 0.2.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except (IOError, ImportError):
    README = ''
 
 setup(name='niprov',
-      version='0.1.post13',
+      version='0.2',
       author='Jasper J.F. van den Bosch',
       author_email='japsai@gmail.com',
       description='provenance for neuroimaging data',

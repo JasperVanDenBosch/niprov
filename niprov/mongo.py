19f471b680c902687e74ff66a38875e0ed9fd34a
Jasper <japsai@gmail.com>
2015-09-14 23:22:08 -0700

    [WIP] Partially implemented new MongoDB Repository.

--- niprov/mongo.py
+++ niprov/mongo.py
@@ -1,7 +1,43 @@
+import pymongo
 from niprov.dependencies import Dependencies
 
 
 class MongoRepository(object):
 
     def __init__(self, dependencies=Dependencies()):
-        pass
+        self.config = dependencies.getConfiguration()
+        self.factory = dependencies.getFileFactory()
+        client = pymongo.MongoClient(self.config.database_url)
+        self.db = client.get_default_database()
+
+    def byPath(self, path):
+        """Get the provenance for a file at the given path. 
+
+        In the case of a dicom series, this returns the provenance for the 
+        series.
+
+        Args:
+            path (str): File system path to the image file.
+
+        Returns:
+            dict: Provenance for one image file.
+        """
+        record = self.db.provenance.find_one({'path':path})
+        return self.factory.fromProvenance(record)
+
+    def knowsByPath(self, path):
+        """Whether the file at this path has provenance associated with it.
+
+        Returns:
+            bool: True if provenance is available for that path.
+        """
+        return self.db.provenance.find_one({'path':path}) is not None
+
+    def knows(self, image):
+        """Whether this file has provenance associated with it.
+
+        Returns:
+            bool: True if provenance is available for this image.
+        """
+        return self.knowsByPath(image.path)
+
---
+++ tests/test_MongoRepo.py
@@ -0,0 +1,45 @@
+import unittest
+from mock import Mock, patch
+
+
+class MongoRepoTests(unittest.TestCase):
+
+    def setUp(self):
+        self.dependencies = Mock()
+        self.config = Mock()
+        self.factory = Mock()
+        self.dependencies.getConfiguration.return_value = self.config
+        self.dependencies.getFileFactory.return_value = self.factory
+        self.db = Mock()
+
+    def setupRepo(self):
+        from niprov.mongo import MongoRepository
+        with patch('niprov.mongo.pymongo') as pymongo:
+            self.repo = MongoRepository(dependencies=self.dependencies)
+        self.repo.db = self.db
+
+    def test_Connection(self):
+        from niprov.mongo import MongoRepository
+        with patch('niprov.mongo.pymongo') as pymongo:
+            repo = MongoRepository(dependencies=self.dependencies)
+        pymongo.MongoClient.assert_called_with(self.config.database_url)
+        self.assertEqual(pymongo.MongoClient().get_default_database(), repo.db)
+
+    def test_knowsByPath(self):
+        self.setupRepo()
+        p = '/p/f1'
+        self.db.provenance.find_one.return_value = None
+        self.assertFalse(self.repo.knowsByPath(p))
+        self.db.provenance.find_one.assert_called_with({'path':p})
+        self.db.provenance.find_one.return_value = 1
+        self.assertTrue(self.repo.knowsByPath(p))
+
+    def test_byPath_returns_img_from_record_with_path(self):
+        self.setupRepo()
+        p = '/p/f1'
+        out = self.repo.byPath(p)
+        self.db.provenance.find_one.assert_called_with({'path':p})
+        self.factory.fromProvenance.assert_called_with(
+            self.db.provenance.find_one())
+        self.assertEqual(self.factory.fromProvenance(), out)
+

5354563eb726b7cbbca99dd7b153685044ab273b
Jasper van den Bosch <jasperb@uw.edu>
2015-09-17 18:39:15 -0700

    [doc] Added docstrings to LocationFactory.

--- niprov/locationfactory.py
+++ niprov/locationfactory.py
@@ -9,7 +9,17 @@ class LocationFactory(object):
         self.dependencies = dependencies
 
     def fromString(self, locationString):
+        """Creates a Location object.
+        
+        Args:
+            locationString (str): String with path and optionally computer id. 
+        """
         return Location(locationString)
 
     def completeString(self, locationString):
+        """Validates a location string.
+        
+        If locationString only contains a filesystem path, computer info will
+        be added.
+        """
         return str(self.fromString(locationString))

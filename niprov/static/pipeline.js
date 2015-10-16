941a74a5e2bdfc7956c38e3aab51f57b2299b66b
Jasper <japsai@gmail.com>
2015-10-16 00:37:16 -0700

    pipeline svg; added labels

--- niprov/static/pipeline.js
+++ niprov/static/pipeline.js
@@ -6,24 +6,32 @@ var svgHeight = 500;
 var dataset = [ 'raw file', 'transform 1', 'transform 2' ];
 
 //Create SVG element
-var svg = d3.select("body")
-            .append("svg")
-            .attr("width", svgWidth)
-            .attr("height", svgHeight);
+var svg = d3.select('body')
+            .append('svg')
+            .attr('width', svgWidth)
+            .attr('height', svgHeight);
 
-var rectangles = svg.selectAll("rect")
+var groups = svg.selectAll('g')
     .data(dataset)
     .enter()
-    .append("rect");
+    .append('g');
 
-rectangles
-    .attr("x", svgWidth/2)
-    .attr("y", function(d, i) {
+groups
+    .append('rect')
+    .attr('x', svgWidth/2)
+    .attr('y', function(d, i) {
         return 20+i*70;
     })
-    .attr("width", 100)
-    .attr("height", 50)
-    .append("text")
+    .attr('width', 100)
+    .attr('height', 50)
+
+groups
+    .append('text')
     .text( function(d) { 
         return d;
+    })
+    .attr('x', svgWidth/2)
+    .attr('y', function(d, i) {
+        return 30+i*70;
     });
+

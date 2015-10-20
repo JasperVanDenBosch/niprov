bd7d10f8d9d69f5b55071fc700cb833152d72ccd
Jasper <japsai@gmail.com>
2015-10-20 00:18:17 -0700

    JsonSerializer now deals with BaseFiles. Pipeline template hands over pipeline files in as json.

--- niprov/jsonfile.py
+++ niprov/jsonfile.py
@@ -14,16 +14,19 @@ class JsonFile(object):
         url = dependencies.getConfiguration().database_url
         self.datafile = os.path.expanduser(url)
 
+    def serializeAndWrite(self, images):
+        jsonstr = self.json.serializeList(images)
+        self.filesys.write(self.datafile, jsonstr)
+
     def add(self, image):
         """Add the provenance for one file to storage.
 
         Args:
             image (:class:`.BaseFile`): Image file to store.
         """
-        current = self._all()
-        current.append(image.provenance)
-        jsonstr = self.json.serializeList(current)
-        self.filesys.write(self.datafile, jsonstr)
+        current = self.all()
+        current.append(image)
+        self.serializeAndWrite(current)
 
     def update(self, image):
         """Save changed provenance for this file..
@@ -31,12 +34,11 @@ class JsonFile(object):
         Args:
             image (:class:`.BaseFile`): Image file that has changed.
         """
-        current = self._all()
+        current = self.all()
         for r in range(len(current)):
-            if current[r]['location'] == image.location.toString():
-                current[r] = image.provenance
-        jsonstr = self.json.serializeList(current)
-        self.filesys.write(self.datafile, jsonstr)
+            if current[r].location.toString() == image.location.toString():
+                current[r] = image
+        self.serializeAndWrite(current)
 
     def all(self):
         """Retrieve all known provenance from storage.
@@ -44,10 +46,6 @@ class JsonFile(object):
         Returns:
             list: List of provenance for known files.
         """
-        records = self._all()
-        return [self.factory.fromProvenance(record) for record in records]
-
-    def _all(self):
         try:
             jsonstr = self.filesys.read(self.datafile)
         except IOError:
@@ -106,18 +104,17 @@ class JsonFile(object):
         Returns:
             dict: Provenance for one image file.
         """
-        for record in self._all():
-            if record['location'] == locationString:
-                return self.factory.fromProvenance(record)
-            elif 'filesInSeries' in record and locationString in record['filesInSeries']:
-                return self.factory.fromProvenance(record)
+        for image in self.all():
+            if image.location.toString() == locationString:
+                return image
+            elif 'filesInSeries' in image.provenance and (
+                locationString in image.provenance['filesInSeries']):
+                return image
         else:
             raise IndexError('No file with that path known.')
 
     def byLocations(self, listOfLocations):
-        all = self._all()
-        records = [f for f in all if f['location'] in listOfLocations]
-        return [self.factory.fromProvenance(record) for record in records]
+        return [f for f in self.all() if f.location.toString() in listOfLocations]
 
     def bySubject(self, subject):
         """Get the provenance for all files of a given participant. 
@@ -128,10 +125,9 @@ class JsonFile(object):
         Returns:
             list: List of provenance for known files imaging this subject.
         """
-        all = self._all()
-        recordsWithSubject = [f for f in all if 'subject' in f]
-        records = [f for f in recordsWithSubject if f['subject']==subject]
-        return [self.factory.fromProvenance(record) for record in records]
+        all = self.all()
+        imagesWithSubject = [f for f in all if 'subject' in f.provenance]
+        return [f for f in imagesWithSubject if f.provenance['subject']==subject]
 
     def getSeries(self, image):
         """Get the object that carries provenance for the series that the image 
@@ -153,13 +149,12 @@ class JsonFile(object):
             raise IndexError('No provenance record for that series.')
 
     def byApproval(self, approvalStatus):
-        allRecords = self._all()
         matches = []
-        for prov in allRecords:
-            if 'approval' in prov:
-                if prov['approval'] == approvalStatus:
-                    matches.append(prov)
-        return [self.factory.fromProvenance(r) for r in matches]
+        for image in self.all():
+            if 'approval' in image.provenance:
+                if image.provenance['approval'] == approvalStatus:
+                    matches.append(image)
+        return matches
 
     def updateApproval(self, fpath, approvalStatus):
         img = self.byLocation(fpath)
@@ -167,30 +162,28 @@ class JsonFile(object):
         self.update(img)
 
     def latest(self, n=20):
-        allRecords = self._all()
-        sortedRecords = sorted(allRecords, key=itemgetter('added'), reverse=True)
-        records = sortedRecords[:n]
-        return [self.factory.fromProvenance(record) for record in records]
+        def dateTimeAdded(img):
+            return img.provenance.get('added')
+        sortedImages = sorted(self.all(), key=dateTimeAdded, reverse=True)
+        return sortedImages[:n]
 
     def statistics(self):
         stats = {}
-        allRecords = self._all()
-        stats['count'] = len(allRecords)
-        stats['totalsize'] = sum([r['size'] for r in allRecords])
+        images = self.all()
+        stats['count'] = len(images)
+        stats['totalsize'] = sum([img.provenance['size'] for img in images])
         return stats
 
     def byId(self, uid):
-        for record in self._all():
-            if record['id'] == uid:
-                return self.factory.fromProvenance(record)
+        for image in self.all():
+            if image.provenance['id'] == uid:
+                return image
         else:
             raise IndexError('No file with that path known.')
 
     def byParents(self, listOfParentLocations):
-        all = self._all()
-        records = [f for f in all if set(listOfParentLocations).intersection(
-            f['parents'])]
-        return [self.factory.fromProvenance(record) for record in records]
+        return [f for f in self.all() if set(listOfParentLocations).intersection(
+            f.provenance.get('parents',[]))]
 
        
 
--- tests/test_jsonfile.py
+++ tests/test_jsonfile.py
@@ -9,31 +9,37 @@ class JsonFileTest(DependencyInjectionTestBase):
         super(JsonFileTest, self).setUp()
         self.dependencies.getConfiguration().database_url = ''
 
-    def test_Update(self):
-        from niprov.jsonfile import JsonFile
-        repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [{'location':'1','path':'a'},
-            {'location':'2','path':'b'}]
-        image = Mock()
-        image.location.toString.return_value = '2'
-        image.provenance = {'foo':'bar'}
-        repo.update(image)
-        self.serializer.serializeList.assert_called_with(
-            [{'location':'1','path':'a'},{'foo':'bar'}])
-        self.filesys.write.assert_called_with(repo.datafile, 
-            self.serializer.serializeList())
+    def imageWithProvenance(self, prov):
+        img = Mock()
+        img.provenance = prov
+        if 'location' in prov:
+            img.location.toString.return_value = prov['location']
+        return img
 
     def test_Add(self):
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [{'location':'1','path':'a'}]
-        image = Mock()
-        image.provenance = {'foo':'bar'}
+        img1 = self.imageWithProvenance({'location':'1','foo':'baz'})
+        repo.all = Mock()
+        repo.all.return_value = [img1]
+        image = self.imageWithProvenance({'location': '2','foo':'bar'})
         repo.add(image)
         self.serializer.serializeList.assert_called_with(
-            [{'location':'1','path':'a'},{'foo':'bar'}])
+            [img1, image])
+        self.filesys.write.assert_called_with(repo.datafile, 
+            self.serializer.serializeList())
+
+    def test_Update(self):
+        from niprov.jsonfile import JsonFile
+        repo = JsonFile(self.dependencies)
+        img1 = self.imageWithProvenance({'location':'1','path':'a'})
+        img2 = self.imageWithProvenance({'location':'2','path':'b'})
+        repo.all = Mock()
+        repo.all.return_value = [img1, img2]
+        image = self.imageWithProvenance({'location': '2','foo':'bar'})
+        repo.update(image)
+        self.serializer.serializeList.assert_called_with(
+            [img1,image])
         self.filesys.write.assert_called_with(repo.datafile, 
             self.serializer.serializeList())
 
@@ -48,24 +54,22 @@ class JsonFileTest(DependencyInjectionTestBase):
     def test_byLocation(self):
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [{'location':'1','path':'a'},
-            {'location':'2','path':'b'}]
-        out = repo.byLocation('1')
-        self.fileFactory.fromProvenance.assert_called_with(
-            {'location':'1','path':'a'})
-        self.assertEqual(self.fileFactory.fromProvenance(), out)
+        img1 = self.imageWithProvenance({'location':'1','path':'a'})
+        img2 = self.imageWithProvenance({'location':'2','path':'b'})
+        repo.all = Mock()
+        repo.all.return_value = [img1, img2]
+        out = repo.byLocation('2')
+        self.assertEqual(img2, out)
 
     def test_byLocation_works_for_file_in_series(self):
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [{'location':'1','path':'a'},
-            {'location':'3','filesInSeries':['boo','bah']}]
+        img1 = self.imageWithProvenance({'location':'1','path':'a'})
+        img3 = self.imageWithProvenance({'location':'3','filesInSeries':['boo','bah']})
+        repo.all = Mock()
+        repo.all.return_value = [img1, img3]
         out = repo.byLocation('boo')
-        self.fileFactory.fromProvenance.assert_called_with(
-            {'location':'3','filesInSeries':['boo','bah']})
-        self.assertEqual(self.fileFactory.fromProvenance(), out)
+        self.assertEqual(img3, out)
 
     def test_updateApproval(self):
         from niprov.jsonfile import JsonFile
@@ -81,57 +85,50 @@ class JsonFileTest(DependencyInjectionTestBase):
         repo.updateApproval('/p/f1','excellent!')
 
     def test_bySubject(self):
-        self.fileFactory.fromProvenance.side_effect = lambda p: 'img_'+p['a']
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [{'subject':'john','a':'b'},
-            {'subject':'tim','a':'d'},{'subject':'john','a':'f'}]
+        img1 = self.imageWithProvenance({'subject':'john','a':'b'})
+        img2 = self.imageWithProvenance({'subject':'tim','a':'d'})
+        img3 = self.imageWithProvenance({'subject':'john','a':'f'})
+        repo.all = Mock()
+        repo.all.return_value = [img1, img2, img3]
         out = repo.bySubject('john')
-        self.fileFactory.fromProvenance.assert_any_call({'subject':'john','a':'b'})
-        self.fileFactory.fromProvenance.assert_any_call({'subject':'john','a':'f'})
-        self.assertEqual(['img_b', 'img_f'], out)
+        self.assertEqual([img1, img3], out)
 
     def test_byApproval(self):
-        self.fileFactory.fromProvenance.side_effect = lambda p: 'img_'+p['a']
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [{'approval':'y','a':'b'},
-            {'approval':'x','a':'d'},{'approval':'x','a':'f'}]
+        img1 = self.imageWithProvenance({'approval':'y','a':'b'})
+        img2 = self.imageWithProvenance({'approval':'x','a':'d'})
+        img3 = self.imageWithProvenance({'approval':'x','a':'f'})
+        repo.all = Mock()
+        repo.all.return_value = [img1, img2, img3]
         out = repo.byApproval('x')
-        self.fileFactory.fromProvenance.assert_any_call({'approval':'x','a':'d'})
-        self.fileFactory.fromProvenance.assert_any_call({'approval':'x','a':'f'})
-        self.assertEqual(['img_d', 'img_f'], out)
+        self.assertEqual([img2, img3], out)
 
     def test_latest(self):
-        self.fileFactory.fromProvenance.side_effect = lambda p: 'img_'+str(p['added'])
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        d1 = datetime.datetime(1982, 1, 5)
-        d2 = datetime.datetime(1982, 2, 5)
-        d3 = datetime.datetime(1982, 3, 5)
-        d4 = datetime.datetime(1982, 4, 5)
-        d5 = datetime.datetime(1982, 5, 5)
-        repo._all = Mock()
-        repo._all.return_value = [{'added':d, 'n':i+1} for i,d in enumerate([d1,d2,d3,d4,d5])]
+        img1 = self.imageWithProvenance({'added':datetime.datetime(1982, 1, 5)})
+        img2 = self.imageWithProvenance({'added':datetime.datetime(1982, 2, 5)})
+        img3 = self.imageWithProvenance({'added':datetime.datetime(1982, 3, 5)})
+        img4 = self.imageWithProvenance({'added':datetime.datetime(1982, 4, 5)})
+        img5 = self.imageWithProvenance({'added':datetime.datetime(1982, 5, 5)})
+        repo.all = Mock()
+        repo.all.return_value = [img1,img2,img3,img4,img5]
         out = repo.latest(3)
-        self.fileFactory.fromProvenance.assert_any_call({'added':d5, 'n':5})
-        self.fileFactory.fromProvenance.assert_any_call({'added':d4, 'n':4})
-        self.fileFactory.fromProvenance.assert_any_call({'added':d3, 'n':3})
-        self.assertEqual(['img_1982-05-05 00:00:00', 'img_1982-04-05 00:00:00',
-            'img_1982-03-05 00:00:00'], out)
+        self.assertEqual([img5, img4, img3], out)
 
     def test_stats(self):
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
+        repo.all = Mock()
         records = [{},{},{},{},{},{},{},{},{},{},{}]
         totalsize = 0
         for r in records:
             r['size'] = random.randint(1,1000)
             totalsize += r['size']
-        repo._all.return_value = records
+        repo.all.return_value = [self.imageWithProvenance(r) for r in records]
         out = repo.statistics()
         self.assertEqual(11, out['count'])
         self.assertEqual(totalsize, out['totalsize'])
@@ -139,47 +136,49 @@ class JsonFileTest(DependencyInjectionTestBase):
     def test_byId(self):
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [{'id':'1','path':'a'},
-            {'id':'2','path':'b'}]
+        img1 = self.imageWithProvenance({'id':'1'})
+        img2 = self.imageWithProvenance({'id':'2'})
+        img3 = self.imageWithProvenance({'id':'3'})
+        repo.all = Mock()
+        repo.all.return_value = [img1, img2, img3]
         out = repo.byId('2')
-        self.fileFactory.fromProvenance.assert_called_with(
-            {'id':'2','path':'b'})
-        self.assertEqual(self.fileFactory.fromProvenance(), out)
+        self.assertEqual(img2, out)
 
     def test_bySubject_doesnt_balk_if_no_subject_field(self):
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [{'subject':'john','a':'b'},
-            {'a':'d'},{'subject':'john','a':'f'}]
+        img1 = self.imageWithProvenance({'a':'b'})
+        img2 = self.imageWithProvenance({'subject':'tim','a':'d'})
+        img3 = self.imageWithProvenance({'subject':'john','a':'f'})
+        repo.all = Mock()
+        repo.all.return_value = [img1, img2, img3]
         out = repo.bySubject('john')
 
     def test_byLocations(self):
         self.fileFactory.fromProvenance.side_effect = lambda p: 'img_'+p['a']
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [{'location':'j','a':'b'},
-            {'location':'l','a':'d'},{'location':'f','a':'f'}]
+        img1 = self.imageWithProvenance({'location':'i'})
+        img2 = self.imageWithProvenance({'location':'j'})
+        img3 = self.imageWithProvenance({'location':'m'})
+        img4 = self.imageWithProvenance({'location':'f'})
+        img5 = self.imageWithProvenance({'location':'x'})
+        repo.all = Mock()
+        repo.all.return_value = [img1,img2,img3,img4,img5]
         out = repo.byLocations(['j','f','k'])
-        self.fileFactory.fromProvenance.assert_any_call({'location':'j','a':'b'})
-        self.fileFactory.fromProvenance.assert_any_call({'location':'f','a':'f'})
-        self.assertEqual(['img_b', 'img_f'], out)
+        self.assertEqual([img2, img4], out)
 
     def test_byParents(self):
         self.fileFactory.fromProvenance.side_effect = lambda p: 'img_'+p['l']
         from niprov.jsonfile import JsonFile
         repo = JsonFile(self.dependencies)
-        repo._all = Mock()
-        repo._all.return_value = [
-            {'parents':[],'l':'a'},
-            {'parents':['x','a'],'l':'b'},
-            {'parents':['b'],'l':'b'},
-            {'parents':['c','y'],'l':'d'},
-            {'parents':['d'],'l':'e'},]
+        img1 = self.imageWithProvenance({'parents':[],'l':'a'})
+        img2 = self.imageWithProvenance({'parents':['x','a'],'l':'b'})
+        img3 = self.imageWithProvenance({'parents':['b'],'l':'b'})
+        img4 = self.imageWithProvenance({'parents':['c','y'],'l':'d'})
+        img5 = self.imageWithProvenance({'parents':['d'],'l':'e'})
+        repo.all = Mock()
+        repo.all.return_value = [img1, img2, img3, img4, img5]
         out = repo.byParents(['x','y'])
-        self.fileFactory.fromProvenance.assert_any_call({'parents':['x','a'],'l':'b'})
-        self.fileFactory.fromProvenance.assert_any_call({'parents':['c','y'],'l':'d'})
-        self.assertEqual(['img_b', 'img_d'], out)
+        self.assertEqual([img2, img4], out)
 

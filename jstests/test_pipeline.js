0668cde24a02a1c4962906731608d8fb1c62a7df
Jasper van den Bosch <jasperb@uw.edu>
2015-11-11 15:24:31 -0800

    Pipeline tree layout implementation done.

--- jstests/test_pipeline.js
+++ jstests/test_pipeline.js
@@ -1,4 +1,4 @@
-var files = [{"added": "2015-10-20T20:50:33.996245", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans1", "transient": true, "parents": ["gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt"], "location": "gram:/p/child1.txt", "path": "/p/child1.txt", "id": "Vspqnf", "subject": "Jane Doe"}, {"added": "2015-10-20T20:49:03.777477", "hash": "f9ccd1b4312d9ebd3daf727054815276", "dimensions": [32, 2080], "created": "2015-03-09T20:19:19.367602", "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transient": false, "location": "gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "path": "/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "size": 492581, "id": "VR55dC", "subject": "Jane Doe"}, {"added": "2015-10-20T20:51:00.178131", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans2", "transient": true, "parents": ["gram:/p/child1.txt"], "location": "gram:/p/b/child2.txt", "path": "/p/b/child2.txt", "id": "toGp6R", "subject": "Jane Doe"}];
+var files = [{"added": "2015-10-20T20:50:33.996245", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans1", "transient": true, "parents": ["gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt"], "location": "gram:/p/child1.txt", "path": "/p/child1.txt", "id": "Vspqnf", "subject": "Jane Doe"}, {"added": "2015-10-20T20:49:03.777477", "hash": "f9ccd1b4312d9ebd3daf727054815276", "dimensions": [32, 2080], "created": "2015-03-09T20:19:19.367602", "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transient": false, "location": "gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "path": "/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "size": 492581, "id": "VR55dC", "subject": "Jane Doe"}, {"added": "2015-10-20T20:51:00.178131", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans2", "transient": true, "parents": ["gram:/p/child1.txt"], "location": "gram:/p/b/child2.txt", "path": "/p/b/child2.txt", "id": "toGp6R", "subject": "Jane Doe"}, {"added": "2015-10-20T20:52:00.178131", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans2", "transient": true, "parents": ["gram:/p/child1.txt"], "location": "gram:/p/b/child3.txt", "path": "/p/b/child3.txt", "id": "J7yUxi", "subject": "Jane Doe"}];
 
 
 QUnit.test( "filesToHierarchy returns object ", function( assert ) {
@@ -7,10 +7,23 @@ QUnit.test( "filesToHierarchy returns object ", function( assert ) {
 });
 
 QUnit.test( "filesToHierarchy root has raw files as children ", function( assert ) {
-    files = [{'n':'a'},{'n':'b','parents':['a']},
-            {'n':'c'},{'n':'d','parents':['c']}]
+    files = [{'location':'a'},{'location':'b','parents':['a']},
+            {'location':'c'},{'location':'d','parents':['c']}]
     var root = filesToHierarchy(files);
-    assert.deepEqual( root, {'children':[{'n':'a'},{'n':'c'}]} );
+    assert.equal( root.children[0].location, 'a' );
+    assert.equal( root.children[1].location, 'c' );
+});
+
+QUnit.test( "filesToHierarchy recursively finds children ", function( assert ) {
+    files = [{'location':'a'},
+            {'location':'b','parents':['a']},{'location':'c','parents':['a']},
+            {'location':'d','parents':['b']},{'location':'e','parents':['b']}]
+    var root = filesToHierarchy(files);
+    assert.equal( root.children[0].location, 'a' );
+    assert.equal( root.children[0].children[0].location, 'b' );
+    assert.equal( root.children[0].children[1].location, 'c' );
+    assert.equal( root.children[0].children[0].children[0].location, 'd' );
+    assert.equal( root.children[0].children[0].children[1].location, 'e' );
 });
 
 QUnit.test( "Translate function returns translate string", function( assert ) {
--- niprov/static/pipeline.js
+++ niprov/static/pipeline.js
@@ -3,21 +3,33 @@
 var filesToHierarchy = function(files) {
     var root = {};
     var rootfiles = files.filter(function(f){ return !('parents' in f) })
+    rootfiles.forEach(function findChildrenRecursively (file, i, arr) {
+        var hasParent = function(file, parentname) {
+            if ('parents' in file) {
+                return file.parents.indexOf(parentname) > -1
+            }
+            else {return false;}
+        };
+        var children = files.filter(function(o){ 
+            return hasParent(o, file.location) });
+        children.forEach(findChildrenRecursively)
+        file.children = children
+    });
     if(rootfiles.length > 0) {
-        root.children = rootfiles
-    }
+        root.children = rootfiles;
+    };
     return root
 };
 var translate = function(x, y) {
     return 'translate(' + x + ',' + y + ')'
-}
+};
 
 //Width and height
 var svgWidth = 500;
 var svgHeight = 500;
 
 //Data
-var files = [{"added": "2015-10-20T20:50:33.996245", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans1", "transient": true, "parents": ["gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt"], "location": "gram:/p/child1.txt", "path": "/p/child1.txt", "id": "Vspqnf", "subject": "Jane Doe"}, {"added": "2015-10-20T20:49:03.777477", "hash": "f9ccd1b4312d9ebd3daf727054815276", "dimensions": [32, 2080], "created": "2015-03-09T20:19:19.367602", "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transient": false, "location": "gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "path": "/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "size": 492581, "id": "VR55dC", "subject": "Jane Doe"}, {"added": "2015-10-20T20:51:00.178131", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans2", "transient": true, "parents": ["gram:/p/child1.txt"], "location": "gram:/p/b/child2.txt", "path": "/p/b/child2.txt", "id": "toGp6R", "subject": "Jane Doe"}];
+var files = [{"added": "2015-10-20T20:50:33.996245", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans1", "transient": true, "parents": ["gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt"], "location": "gram:/p/child1.txt", "path": "/p/child1.txt", "id": "Vspqnf", "subject": "Jane Doe"}, {"added": "2015-10-20T20:49:03.777477", "hash": "f9ccd1b4312d9ebd3daf727054815276", "dimensions": [32, 2080], "created": "2015-03-09T20:19:19.367602", "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transient": false, "location": "gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "path": "/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "size": 492581, "id": "VR55dC", "subject": "Jane Doe"}, {"added": "2015-10-20T20:51:00.178131", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans2", "transient": true, "parents": ["gram:/p/child1.txt"], "location": "gram:/p/b/child2.txt", "path": "/p/b/child2.txt", "id": "toGp6R", "subject": "Jane Doe"}, {"added": "2015-10-20T20:52:00.178131", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans2", "transient": true, "parents": ["gram:/p/child1.txt"], "location": "gram:/p/b/child3.txt", "path": "/p/b/child3.txt", "id": "J7yUxi", "subject": "Jane Doe"}];
 
 //Create SVG element
 var svg = d3.select('body')

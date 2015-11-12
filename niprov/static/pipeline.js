d2069684b67477317d4442e5b3654ffad7510353
Jasper van den Bosch <jasperb@uw.edu>
2015-11-11 17:31:26 -0800

    Pipeline viz has tooltip.

--- niprov/static/pipeline.js
+++ niprov/static/pipeline.js
@@ -38,6 +38,11 @@ var svgHeight = 500;
 //Data
 var files = [{"added": "2015-10-20T20:50:33.996245", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans1", "transient": true, "parents": ["gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt"], "location": "gram:/p/child1.txt", "path": "/p/child1.txt", "id": "Vspqnf", "subject": "Jane Doe"}, {"added": "2015-10-20T20:49:03.777477", "hash": "f9ccd1b4312d9ebd3daf727054815276", "dimensions": [32, 2080], "created": "2015-03-09T20:19:19.367602", "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transient": false, "location": "gram:/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "path": "/home/jasper/Projects/niprov/testdata/eeg/stub.cnt", "size": 492581, "id": "VR55dC", "subject": "Jane Doe"}, {"added": "2015-10-20T20:51:00.178131", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans2", "transient": true, "parents": ["gram:/p/child1.txt"], "location": "gram:/p/b/child2.txt", "path": "/p/b/child2.txt", "id": "toGp6R", "subject": "Jane Doe"}, {"added": "2015-10-20T20:52:00.178131", "script": null, "acquired": "2015-03-09T13:07:03.000000", "hostname": "gram", "transformation": "trans2", "transient": true, "parents": ["gram:/p/child1.txt"], "location": "gram:/p/b/child3.txt", "path": "/p/b/child3.txt", "id": "J7yUxi", "subject": "Jane Doe"}];
 
+// Creat toolip container
+var tooltipDiv = d3.select('body')
+    .append('div')
+    .attr('class','tooltip')
+
 //Create SVG element
 var svg = d3.select('body')
             .append('svg')
@@ -61,14 +66,34 @@ var nodeGroup = svg.selectAll('g.node')
         .attr('transform', function(d) {return translate(d.x, d.y)});
 
 nodeGroup
-    .append('rect')
-    .attr('width', 100)
-    .attr('height', 30);
+    .append('circle')
+    .attr('r', 5);
 
 nodeGroup
     .append('text')
     .text( function(d) { 
         return shortname(d.path);
+    })
+    .attr('transform',translate(10,5))
+    .on("mouseover", function () {
+        
+        var matrix = this.getScreenCTM()
+                .translate(+this.getAttribute("cx"),
+                         +this.getAttribute("cy"));
+        tooltipDiv
+            .text('Hallo!')
+            .style("opacity", "1")
+            .style("left", 
+                   (window.pageXOffset + matrix.e) + "px")
+            .style("top",
+                   (window.pageYOffset + matrix.f + 30) + "px")
+//            .insert('dl').selectAll()
+//                .append('dt').text('key')
+//                .append('dd').text('my value')
+        
+    })
+    .on("mouseout", function () {
+        return tooltipDiv.style("opacity", "0");
     });
 
 svg.selectAll("path.link")
@@ -77,6 +102,8 @@ svg.selectAll("path.link")
     .append('path')
         .attr('class','link')
         .attr('d', d3.svg.diagonal());
+
+
 
 
 

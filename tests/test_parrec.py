96e745f94e8e62d6daf5ab69be9d928fa651a1d7
Jasper van den Bosch <jasperb@uw.edu>
2015-12-01 15:13:44 -0800

    Parrec supports more provenance fields

--- tests/test_parrec.py
+++ tests/test_parrec.py
@@ -25,13 +25,68 @@ class ParrecTests(BaseFileTests):
         out = self.file.inspect()
         self.assertEqual(out['dimensions'], [80,80,10])
 
+    def test_Gets_advanced_fields(self):
+        out = self.file.inspect()
+        self.assertEqual(out['technique'], 'T1TFE')
+        self.assertEqual(out['repetition-time'], 4.364)
+        self.assertEqual(out['field-of-view'], [130., 100., 154.375])
+        self.assertEqual(out['epi-factor'], 1)
+        self.assertEqual(out['magnetization-transfer-contrast'], False)
+        self.assertEqual(out['diffusion'], False)
+        # per-image
+        self.assertEqual(out['slice-thickness'], 10.0)
+        self.assertEqual(out['slice-orientation'], 1)
+        self.assertEqual(out['echo-time'], 2.0800000000000001)
+        self.assertEqual(out['flip-angle'], 8.0)
+        self.assertEqual(out['inversion-time'], 0.0)
+
     def setupNibabel(self):
+        import numpy
         img = Mock()
         img.header.general_info = {
-            'exam_date':'2014.08.05 / 11:27:34',
-            'protocol_name':'T1 SENSE',
-            'patient_name':'John Doeish'}
+             'acq_nr': 6,
+             'angulation': numpy.array([-1.979,  0.546,  0.019]),
+             'diffusion': 0,
+             'diffusion_echo_time': 0.0,
+             'dyn_scan': 0,
+             'epi_factor': 1,
+             'exam_date': '2014.08.05 / 11:27:34',
+             'exam_name': 'test',
+             'flow_compensation': 0,
+             'fov': numpy.array([ 130.   ,  100.   ,  154.375]),
+             'max_cardiac_phases': 1,
+             'max_diffusion_values': 1,
+             'max_dynamics': 1,
+             'max_echoes': 1,
+             'max_gradient_orient': 1,
+             'max_mixes': 1,
+             'max_slices': 10,
+             'mtc': 0,
+             'nr_label_types': 0,
+             'off_center': numpy.array([-18.805,  22.157, -17.977]),
+             'patient_name': 'John Doeish',
+             'patient_position': 'Head First Supine',
+             'phase_enc_velocity': numpy.array([ 0.,  0.,  0.]),
+             'prep_direction': 'Right-Left',
+             'presaturation': 0,
+             'protocol_name': 'T1 SENSE',
+             'recon_nr': 1,
+             'repetition_time': 4.364,
+             'scan_duration': 65.0,
+             'scan_mode': '3D',
+             'scan_resolution': numpy.array([76, 62]),
+             'series_type': 'Image   MRSERIES',
+             'spir': 0,
+             'tech': 'T1TFE',
+             'water_fat_shift': 1.117}
+        img.header.image_defs = numpy.array([ (1, 1, 1, 1, 0, 2, 0, 16, 81, [80, 80], 0.0, 1.26032, 2.84925e-05, 133, 231, [-1.98, 0.55, 0.02], [-18.79, -22.82, -16.42], 10.0, 0.0, 0, 1, 0, 2, [1.912, 1.912], 2.08, 0.0, 0.0, 0.0, 1, 8.0, 0, 0, 0, 7, 0.0, 1, 1, '7', '0', [0.0, 0.0, 0.0], 1),
+       (2, 1, 1, 1, 0, 2, 1, 16, 81, [80, 80], 0.0, 1.26032, 2.84925e-05, 294, 512, [-1.98, 0.55, 0.02], [-18.79, -12.82, -16.77], 10.0, 0.0, 0, 1, 0, 2, [1.912, 1.912], 2.08, 0.0, 0.0, 0.0, 1, 8.0, 0, 0, 0, 7, 0.0, 1, 1, '7', '0', [0.0, 0.0, 0.0], 1),
+       (3, 1, 1, 1, 0, 2, 2, 16, 81, [80, 80], 0.0, 1.26032, 2.84925e-05, 427, 742, [-1.98, 0.55, 0.02], [-18.8, -2.83, -17.11], 10.0, 0.0, 0, 1, 0, 2, [1.912, 1.912], 2.08, 0.0, 0.0, 0.0, 1, 8.0, 0, 0, 0, 7, 0.0, 1, 1, '7', '0', [0.0, 0.0, 0.0], 1)], 
+      dtype=[('slice number', '<i8'), ('echo number', '<i8'), ('dynamic scan number', '<i8'), ('cardiac phase number', '<i8'), ('image_type_mr', '<i8'), ('scanning sequence', '<i8'), ('index in REC file', '<i8'), ('image pixel size', '<i8'), ('scan percentage', '<i8'), ('recon resolution', '<i8', (2,)), ('rescale intercept', '<f8'), ('rescale slope', '<f8'), ('scale slope', '<f8'), ('window center', '<i8'), ('window width', '<i8'), ('image angulation', '<f8', (3,)), ('image offcentre', '<f8', (3,)), ('slice thickness', '<f8'), ('slice gap', '<f8'), ('image_display_orientation', '<i8'), ('slice orientation', '<i8'), ('fmri_status_indication', '<i8'), ('image_type_ed_es', '<i8'), ('pixel spacing', '<f8', (2,)), ('echo_time', '<f8'), ('dyn_scan_begin_time', '<f8'), ('trigger_time', '<f8'), ('diffusion_b_factor', '<f8'), ('number of averages', '<i8'), ('image_flip_angle', '<f8'), ('cardiac frequency', '<i8'), ('minimum RR-interval', '<i8'), ('maximum RR-interval', '<i8'), ('TURBO factor', '<i8'), ('Inversion delay', '<f8'), ('diffusion b value number', '<i8'), ('gradient orientation number', '<i8'), ('contrast type', 'S30'), ('diffusion anisotropy type', 'S30'), ('diffusion', '<f8', (3,)), ('label type', '<i8')])
         img.shape = (80,80,10)
         self.libs.nibabel.load.return_value = img
         self.libs.hasDependency.return_value = True
+
+
+
 

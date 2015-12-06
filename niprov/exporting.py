3da8ee034578406fe7ee7c31dcec362763d1f248
Jasper <japsai@gmail.com>
2015-12-06 14:59:32 -0800

    Export adds records one by one to temp repo.

--- niprov/exporting.py
+++ niprov/exporting.py
@@ -3,12 +3,21 @@ from niprov.jsonfile import JsonFile
 
 
 def export(dependencies=Dependencies()):
+    """Save all current provenance to a file in the current working directory.
+
+    This can serve as a backup, migration tool, or for exchange.
+    """
     exportDeps = Dependencies()
     exportDeps.getConfiguration().database_url = 'provenance.json'
     exportRepo = JsonFile(exportDeps)
     allFiles = dependencies.getRepository().all()
-    exportRepo.addMany(allFiles)
+    for pfile in allFiles:
+        exportRepo.add(pfile)
 
 
 def importp(filepath, dependencies=Dependencies()):
+    """Add provenance in bulk from a file, such as saved by export().
+
+    This can serve as a backup, migration tool, or for exchange.
+    """
     pass
--- tests/test_exporting.py
+++ tests/test_exporting.py
@@ -9,13 +9,14 @@ class ExportingTest(DependencyInjectionTestBase):
 
     def test_export(self):
         import niprov.exporting 
-        self.repo.all.return_value = sentinel.all_records_from_repo
+        self.repo.all.return_value = [sentinel.p1, sentinel.p2]
         niprov.exporting.JsonFile = self.patchJsonFileConstructor()
         niprov.exporting.export(dependencies=self.dependencies)
         assert self.JsonFileCtr.called, "Did not create a JsonFile repo."
         urlUsed = self.tempRepo.dependencies.getConfiguration().database_url
         self.assertEqual(urlUsed, 'provenance.json')
-        self.tempRepo.addMany.assert_called_with(sentinel.all_records_from_repo)
+        self.tempRepo.add.assert_any_call(sentinel.p1)
+        self.tempRepo.add.assert_any_call(sentinel.p2)
 
     def patchJsonFileConstructor(self):
         self.tempRepo = Mock()

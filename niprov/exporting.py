5d5bf7cccb7cc7061bd6db80f7b7699af7ecf630
Jasper <japsai@gmail.com>
2015-12-06 13:15:44 -0800

    Implemented export()

--- niprov/exporting.py
+++ niprov/exporting.py
@@ -1,8 +1,14 @@
 from niprov.dependencies import Dependencies
+from niprov.jsonfile import JsonFile
 
 
 def export(dependencies=Dependencies()):
-    pass
+    exportDeps = Dependencies()
+    exportDeps.getConfiguration().database_url = 'provenance.json'
+    exportRepo = JsonFile(exportDeps)
+    allFiles = dependencies.getRepository().all()
+    exportRepo.addMany(allFiles)
+
 
 def importp(filepath, dependencies=Dependencies()):
     pass
---
+++ tests/test_exporting.py
@@ -0,0 +1,30 @@
+from mock import Mock, patch, sentinel
+from tests.ditest import DependencyInjectionTestBase
+
+
+class ExportingTest(DependencyInjectionTestBase):
+
+    def setUp(self):
+        super(ExportingTest, self).setUp()
+
+    def test_export(self):
+        import niprov.exporting 
+        self.repo.all.return_value = sentinel.all_records_from_repo
+        niprov.exporting.JsonFile = self.patchJsonFileConstructor()
+        niprov.exporting.export(dependencies=self.dependencies)
+        assert self.JsonFileCtr.called, "Did not create a JsonFile repo."
+        urlUsed = self.tempRepo.dependencies.getConfiguration().database_url
+        self.assertEqual(urlUsed, 'provenance.json')
+        self.tempRepo.addMany.assert_called_with(sentinel.all_records_from_repo)
+
+    def patchJsonFileConstructor(self):
+        self.tempRepo = Mock()
+        self.JsonFileCtr = Mock()
+        def ctr(dependencies): 
+            self.tempRepo.dependencies = dependencies
+            return self.tempRepo
+        self.JsonFileCtr.side_effect = ctr
+        return self.JsonFileCtr
+
+
+

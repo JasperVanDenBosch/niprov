2bcd47dd530aaf41b65eaf5e6519e68f5eaeb0fd
Jasper <japsai@gmail.com>
2015-12-28 18:23:54 -0800

    Unit tests for XML format.

---
+++ tests/test_xmlformat.py
@@ -0,0 +1,11 @@
+import unittest
+from mock import Mock
+
+
+class XmlFormatTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_export_item_returns_string_with_xml_header(self):
+        pass

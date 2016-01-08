4767e850c241480685e6d01220183e3ac50c77cc
Jasper van den Bosch <jasperb@uw.edu>
2016-01-07 18:27:56 -0800

    Renamed exporting module to importing so that report can be called export hereonafter

--- niprov/importing.py
+++ niprov/importing.py
@@ -2,24 +2,6 @@ from niprov.dependencies import Dependencies
 from niprov.jsonfile import JsonFile
 from datetime import datetime as dt
 
-
-def export(dependencies=Dependencies()):
-    """Save all current provenance to a file in the current working directory.
-
-    This can serve as a backup, migration tool, or for exchange.
-    """
-    nowstr = dependencies.getClock().getNowString()
-    filepath = 'provenance_export_{0}.json'.format(nowstr)
-    repository = dependencies.getRepository()
-    exportDeps = Dependencies()
-    exportDeps.getConfiguration().database_url = filepath
-    exportRepo = JsonFile(exportDeps)
-    allFiles = repository.all()
-    for pfile in allFiles:
-        exportRepo.add(pfile)
-    return filepath
-
-
 def importp(filepath, dependencies=Dependencies()):
     """Add provenance in bulk from a file, such as saved by export().
 

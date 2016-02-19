4f63d1eb04d4629bd7c7399b044d310f40919421
Jasper van den Bosch <jasperb@uw.edu>
2016-02-18 16:46:31 -0800

    Added a script to test templatge rendering of dictionary value fields.

---
+++ scripts/dictionary-fields.py
@@ -0,0 +1,7 @@
+import niprov.adding
+provdict = {'mydict':{'a':1234567,'bbbbbbb':'Hello. I was wondering..'}}
+(rawbold, s) = niprov.adding.add('testdata/parrec/T2.PAR', 
+    provenance=provdict)
+
+
+

8e5edf789cd335700cc4c8bfa90474ef6bdb22f2
Jasper van den Bosch <jasperb@uw.edu>
2016-03-09 18:19:43 -0800

    Determining user based on OS, config or passed value

--- niprov/users.py
+++ niprov/users.py
@@ -1,4 +1,14 @@
 import getpass
 
-def determineUser():
-    return getpass.getuser()
+class Users(object):
+
+    def __init__(self, dependencies):
+        self.config = dependencies.getConfiguration()
+
+    def determineUser(self, passedUserValue):
+        if passedUserValue is not None:
+            return passedUserValue
+        if self.config.user:
+            return self.config.user
+        else:
+            return getpass.getuser()
--- tests/test_users.py
+++ tests/test_users.py
@@ -8,9 +8,23 @@ class UsersTests(DependencyInjectionTestBase):
 
     def setUp(self):
         super(UsersTests, self).setUp()
+        self.config.user = None
 
     def test_determineUser_if_not_config_or_runtime_uses_OS(self):
-        from niprov.users import determineUser
+        from niprov.users import Users
+        users = Users(self.dependencies)
         import getpass
-        self.assertEqual(determineUser(), getpass.getuser())
+        self.assertEqual(users.determineUser(None), getpass.getuser())
+
+    def test_determineUser_if_not_runtime_uses_config(self):
+        from niprov.users import Users
+        users = Users(self.dependencies)
+        self.config.user = 'jasmine'
+        self.assertEqual(users.determineUser(None), 'jasmine')
+
+    def test_determineUser_if_runtime_given_uses_that(self):
+        from niprov.users import Users
+        users = Users(self.dependencies)
+        self.config.user = 'jasmine'
+        self.assertEqual(users.determineUser('oddyseus'), 'oddyseus')
 

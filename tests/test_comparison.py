5717438c8cfecea6660eefee607c749406097ded
Jasper van den Bosch <jasperb@uw.edu>
2016-04-04 14:13:19 -0700

    Test for comparison feature

---
+++ tests/test_comparison.py
@@ -0,0 +1,11 @@
+import unittest, os
+from mock import Mock, patch, call
+from tests.ditest import DependencyInjectionTestBase
+
+
+class ComparisonTests(DependencyInjectionTestBase):
+
+    def setUp(self):
+        super(ComparisonTests, self).setUp()
+
+

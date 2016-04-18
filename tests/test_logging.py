17d13f2a9f814b06648220821d77be300e633215
Jasper <japsai@gmail.com>
2016-04-17 21:07:43 -0700

    Unit test for log determining the user.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -84,6 +84,12 @@ class LoggingTests(DependencyInjectionTestBase):
         self.assertEqual(self.provenancesCreated[0]['code'],'abc')
         self.assertEqual(self.provenancesCreated[0]['logtext'],'def')
 
+    def test_Determines_user_and_logs_them(self):
+        self.dependencies.users.determineUser.return_value = 'paprika'
+        self.log('new', 'trans', 'old', user='mononoko')
+        self.dependencies.users.determineUser.assert_called_with('mononoko')
+        self.assertEqual(self.provenancesCreated[0]['user'],'paprika')
+
     def test_Script_added_to_provenance(self):
         parents = ['/p/f1']
         new = '/p/f2'

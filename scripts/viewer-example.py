02a98d7ba9e00750b9d8fc6abbd75621977c98db
Jasper van den Bosch <jasperb@uw.edu>
2016-05-17 16:43:45 -0700

    Snapshots and views of images

---
+++ scripts/viewer-example.py
@@ -0,0 +1,10 @@
+from niprov import Context
+provenance = Context()
+(rawstru, s) = provenance.add('testdata/dicom/T1.dcm')
+
+# img.view()        # show in window and return
+# img.snapshot()    # save png to cwd
+
+
+
+

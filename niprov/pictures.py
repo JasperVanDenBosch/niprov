f4b59782909a910338ea02aa21daba0604333d8b
Jasper <japsai@gmail.com>
2016-05-19 20:09:28 -0700

    Dummy PictureCache class.

--- niprov/pictures.py
+++ niprov/pictures.py
@@ -4,3 +4,7 @@ class PictureCache(object):
 
     def __init__(self, dependencies):
         pass
+
+    def serialize(self, image):
+        pass
+
---
+++ tests/test_pictures.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from mock import Mock, sentinel, patch
+
+
+class PictureCacheTests(TestCase):
+
+    def test_Serialize(self):
+        from niprov.pictures import PictureCache
+        pictures = PictureCache(sentinel.dependencies)
+        pictures.serialize(sentinel.img)
+

d6c24c6b820f2b2177bb8d12192ba1651f18651a
Jasper <japsai@gmail.com>
2016-05-20 11:43:18 -0700

    drafted tests for PictureCache

--- tests/test_pictures.py
+++ tests/test_pictures.py
@@ -9,3 +9,14 @@ class PictureCacheTests(TestCase):
         pictures = PictureCache(sentinel.dependencies)
         pictures.serializeSingle(sentinel.img)
 
+    def test_Provides_new_picture_file_handle(self):
+        pass
+
+    def test_Stored_picture_can_be_retrieved_as_filepath(self):
+        # store(fhandle) - > serialize() / getPictureFilepathFor()
+        pass
+
+    def test_Stored_picture_can_be_retrieved_as_bytes(self):
+        # store(fhandle) - > getPictureDataFor()
+        pass
+

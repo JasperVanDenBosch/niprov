56d6e142b211f07bcff72787cb142dc417e81ef8
Jasper van den Bosch <jasperb@uw.edu>
2016-06-07 15:39:11 -0700

    Query byProject, byUser

--- niprov/querying.py
+++ niprov/querying.py
@@ -3,11 +3,20 @@ QueryField = namedtuple('QueryField', ['name', 'value'])
 
 class Query(object):
 
-    fields = []
+    def __init__(self):
+        self.fields = []
+
+    def getFields(self):
+        return self.fields
 
     def byModality(self, val):
         self.fields.append(QueryField('modality', val))
         return self
 
-    def getFields(self):
-        return self.fields
+    def byProject(self, val):
+        self.fields.append(QueryField('project', val))
+        return self
+
+    def byUser(self, val):
+        self.fields.append(QueryField('user', val))
+        return self
--- tests/test_query.py
+++ tests/test_query.py
@@ -14,6 +14,18 @@ class QueryTest(DependencyInjectionTestBase):
         self.assertEqual('modality', q.getFields()[0].name)
         self.assertEqual('magic', q.getFields()[0].value)
 
-        
-    
+    def test_byProject(self):
+        from niprov.querying import Query
+        q = Query().byProject('sealion')
+        self.assertEqual(1, len(q.getFields()))
+        self.assertEqual('project', q.getFields()[0].name)
+        self.assertEqual('sealion', q.getFields()[0].value)
+
+    def test_byUser(self):
+        from niprov.querying import Query
+        q = Query().byUser('dumbledore')
+        self.assertEqual(1, len(q.getFields()))
+        self.assertEqual('user', q.getFields()[0].name)
+        self.assertEqual('dumbledore', q.getFields()[0].value)
+
 

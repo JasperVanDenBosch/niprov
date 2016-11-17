845460856981d9d1027dc85185a657acb783000e
Jasper van den Bosch <japsai@gmail.com>
2016-11-16 18:49:54 -0800

    Fixed the viewer script; view() expects a BaseFile object.

--- scripts/viewer-example.py
+++ scripts/viewer-example.py
@@ -2,5 +2,5 @@ import os
 from niprov import ProvenanceContext
 provenance = ProvenanceContext()
 img = provenance.add('testdata/nifti/qt1.nii.gz')
-provenance.view(os.path.abspath('testdata/nifti/qt1.nii.gz'))
+provenance.view(img)
 img.viewSnapshot()
